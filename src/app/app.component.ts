045506bde9ebd8fb16b71e0b73f106f43b5163d1
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2023-07-19 18:24:26 +0300

    ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  color = "green1";
   title = 'youtube';
   // @ts-ignore
   getVal(item){
