1587100bf6e3436ad63643bc7e67c0e20848ba67
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2023-07-19 18:15:28 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'youtube';
+  // @ts-ignore
+  getVal(item){
+    console.log(item.target.value);
+  }
+  // @ts-ignore
+  getTextBox(txt) {
+  console.log(txt.value)
+  }
+  // @ts-ignore
+  testFunc(a){
+    alert(a);
+  }
+}
