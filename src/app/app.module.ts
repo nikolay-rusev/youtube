c633bed3c42f4a87569aa456973dbc14f4da8692
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2023-07-25 11:54:10 +0300

    Angular 9, tutorial #11: create some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { UsersModule } from './users/users.module'
-import {FormsModule} from "@angular/forms"
+import {FormsModule} from "@angular/forms";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
