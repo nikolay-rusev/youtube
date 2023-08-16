21e4c907a91f15e6982133a722ac9ea93dd42e19
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2023-08-16 13:12:47 +0300

    Angular 9 tutorial # make Services with example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private userData: UserService) {}
+  ngOnInit() {
+    this.userData.getData();
+  }
+
+  callService(){
+    this.userData.getData();
+  }
+}
